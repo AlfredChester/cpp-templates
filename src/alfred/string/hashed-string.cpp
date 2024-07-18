d3fd3972714585277dcd6c0b8e140f75a0792714
FredB-mine <fredbao1126@gmail.com>
2024-07-18 09:22:05 +0800

    Improved constant efficiency of HashedString

--- src/alfred/string/hashed-string.cpp
+++ src/alfred/string/hashed-string.cpp
@@ -1,39 +1,38 @@
-using i64 = long long;
-using i128 = __int128;
-class HashedString {
-private:
-    // change M and B if you want
-    static const i64 M = (1LL << 61) - 1;
-    static const i64 B;
-    // pow[i] contains B^i % M
-    static std::vector<i64> pow;
-    // p_hash[i] is the hash of the first i characters of the given string
-    std::vector<i64> r_hash, p_hash;
-    i128 mul(i64 a, i64 b) { return (i128)a * b; }
-    i64 mod_mul(i64 a, i64 b) { return mul(a, b) % M; }
-
-public:
-    HashedString(const string &s) : r_hash(s.size() + 1), p_hash(s.size() + 1) {
-        while (pow.size() < s.size()) { pow.push_back(mod_mul(pow.back(), B)); }
-        p_hash[0] = 0;
-        r_hash[0] = 0;
-        for (size_t i = 0; i < s.size(); i++) {
-            p_hash[i + 1] = (mul(p_hash[i], B) + s[i]) % M; // 1-based
-        }
-        i64 sz = s.size();
-        for (int i = sz - 1, j = 0; i >= 0; i--, j++) {
-            r_hash[j + 1] = (mul(r_hash[j], B) + s[i]) % M;
+template <int mod, int seed>
+struct SingleHash {
+    int n;
+    std::vector<int> pow, h;
+    SingleHash(void) = default;
+    SingleHash(std::string &s) { init(s); }
+    inline void init(std::string &s) {
+        n = s.size(), h.assign(n + 2, 0), pow.assign(n + 2, 1);
+        for (int i = 1; i <= n; i++) {
+            pow[i] = 1ll * pow[i - 1] * seed % mod;
+            h[i] = (1ll * h[i - 1] * seed + s[i - 1]) % mod;
         }
     }
-    i64 getHash(int start, int end) { // 0 based
-        i64 raw_val = p_hash[end + 1] - mod_mul(p_hash[start], pow[end - start + 1]);
-        return (raw_val + M) % M;
+    inline int get_hash(int l, int r) {
+        return (h[r + 1] - 1ll * h[l] * pow[r - l + 1] % mod + mod) % mod;
     }
-    i64 getRHash(int start, int end) { // 0 based
-        i64 raw_val = r_hash[end + 1] - mod_mul(r_hash[start], pow[end - start + 1]);
-        return (raw_val + M) % M;
+    inline bool check_same(int l1, int r1, int l2, int r2) {
+        return get_hash(l1, r1) == get_hash(l2, r2);
     }
 };
-std::vector<i64> HashedString::pow = {1};
-mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
-const i64 HashedString::B = uniform_int_distribution<i64>(0, M - 1)(rng);
+struct HashedString {
+    SingleHash<998244353, 477> H1;
+    SingleHash<1000000007, 233> H2;
+    HashedString(void) = default;
+    HashedString(std::string &s) : H1(s), H2(s) {}
+    inline void init(std::string &s) {
+        H1.init(s), H2.init(s);
+    }
+    std::pair<int, int> get_hash(int l, int r) { // not recommended.
+        return {H1.get_hash(l, r), H2.get_hash(l, r)};
+    }
+    inline bool check_same(int l1, int r1, int l2, int r2) {
+        return H1.check_same(l1, r1, l2, r2) && H2.check_same(l1, r1, l2, r2);
+    }
+    inline bool check_period(int l, int r, int p) {
+        return check_same(l, r - p, l + p, r);
+    }
+};

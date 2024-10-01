4dcbad514dc4c2eaaf8f737f5d734afa1b81d6a3
FredB-mine <fredbao1126@gmail.com>
2024-10-01 15:06:24 +0800

    Include discretization.h in appear-statistics.cpp.

--- src/alfred/data_structure/appear-statistics.cpp
+++ src/alfred/data_structure/appear-statistics.cpp
@@ -1,23 +1,5 @@
+#include "discretization.h"
 #include <bits/stdc++.h>
-
-template <class _Tp>
-struct Mess {
-    std::vector<_Tp> v;
-    bool initialized = false;
-    inline _Tp origin(int idx) { return v[idx - 1]; }
-    inline void insert(_Tp x) { v.push_back(x); }
-    template <typename T, typename... V>
-    inline void insert(T x, V... v) { insert(x), insert(v...); }
-    inline void init(void) {
-        sort(v.begin(), v.end()), initialized = true;
-        v.erase(unique(v.begin(), v.end()), v.end());
-    }
-    inline int query(_Tp x) {
-        if (!initialized) init();
-        return lower_bound(v.begin(), v.end(), x) - v.begin() + 1;
-    }
-    inline bool exist(_Tp x) { return origin(query(x)) == x; }
-};
 
 template <class T>
 class AppearStats { // Appear Statistics.

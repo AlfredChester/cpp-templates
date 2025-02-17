87bb46e867e2b887426c0aa7f9a71b2724a22908
FredB-mine <fredbao1126@gmail.com>
2025-02-18 00:02:55 +0800

    Final commit

--- pdf/compose.py
+++ pdf/compose.py
@@ -1,32 +1,32 @@
-import os
+# import os
 
-WARNING = """
-WARNING: This script is only guaranteed to work
-         on alfred's macBook. It may fail or cause 
-         unexpected effects on other devices. It is
-         suggested to view the source code before
-         running.
-"""
+# WARNING = """
+# WARNING: This script is only guaranteed to work
+#          on alfred's macBook. It may fail or cause
+#          unexpected effects on other devices. It is
+#          suggested to view the source code before
+#          running.
+# """
 
-CHECK = "Sure to continue? (y/N)"
-
-
-def checkUser() -> bool:
-    try:
-        return os.environ["USER"] == "alfred"
-    except:
-        return False
+# CHECK = "Sure to continue? (y/N)"
 
 
-def main() -> int:
-    if not checkUser():
-        print(WARNING)
-        res = input(CHECK)
-        if res != "Y" and res != "y":
-            return 0
-    print("Composing to cpp.json")
-    return 0
+# def checkUser() -> bool:
+#     try:
+#         return os.environ["USER"] == "alfred"
+#     except:
+#         return False
 
 
-if __name__ == "__main__":
-    main()
+# def main() -> int:
+#     if not checkUser():
+#         print(WARNING)
+#         res = input(CHECK)
+#         if res != "Y" and res != "y":
+#             return 0
+#     print("Composing to cpp.json")
+#     return 0
+
+
+# if __name__ == "__main__":
+#     main()

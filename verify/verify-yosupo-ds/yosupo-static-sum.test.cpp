4b805bb4d2048b74db46fe1802637c91a7ad9d7d
FredB-mine <fredbao1126@gmail.com>
2025-02-18 18:50:53 +0800

    static sum test.

---
+++ verify/verify-yosupo-ds/yosupo-static-sum.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+
+#include "../../src/alfred/config/io-sync-off.hpp"
+#include "../../src/alfred/data_structure/prefix-sum.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    int n, q, l, r;
+    optimizeIO(), std::cin >> n >> q;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    Sum<long long> S(a);
+    while (q--) {
+        std::cin >> l >> r;
+        std::cout << S.query(l, r - 1) << '\n';
+    }
+
+    return 0;
+}

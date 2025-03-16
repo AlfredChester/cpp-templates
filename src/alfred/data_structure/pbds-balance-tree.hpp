0b5c52a5d22d929c68ca578ca93927f73f8d17db
FredB-mine <fredbao1126@gmail.com>
2025-03-16 22:40:01 +0800

    Avoid namespace pollution

--- src/alfred/data_structure/pbds-balance-tree.hpp
+++ src/alfred/data_structure/pbds-balance-tree.hpp
@@ -3,9 +3,6 @@
 #include <bits/extc++.h>
 #include <bits/stdc++.h>
 
-using namespace std;
-using namespace __gnu_pbds;
-
 // TreeTag can also be __gnu_pbds::splay_tree_tag
-template <class T, class Cmp, class TreeTag = rb_tree_tag>
-using BalanceTree = tree<T, null_type, Cmp, TreeTag, tree_order_statistics_node_update>;
+template <class T, class Cmp, class TreeTag = __gnu_pbds::rb_tree_tag>
+using BalanceTree = __gnu_pbds::tree<T, __gnu_pbds::null_type, Cmp, TreeTag, __gnu_pbds::tree_order_statistics_node_update>;

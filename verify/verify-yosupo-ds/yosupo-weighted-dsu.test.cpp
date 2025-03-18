97a60c70a4b7df071effb7b76f158d5d8d150225
FredB-mine <fredbao1126@gmail.com>
2025-03-18 23:35:37 +0800

    Verify weighted DSU.

---
+++ verify/verify-yosupo-ds/yosupo-weighted-dsu.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind_with_potential"
+
+#include "../../src/alfred/config/io-sync-off.hpp"
+#include "../../src/alfred/data_structure/weighted-dsu.hpp"
+#include "../../src/alfred/math/mod-int.hpp"
+#include <iostream>
+
+int n, q, opt, u, v, x;
+using mint = ModInt<998244353>;
+
+int main(int argc, char const *argv[]) {
+    optimizeIO(), std::cin >> n >> q;
+
+    WeightedDSU<mint> dsu(n);
+
+    while (q--) {
+        std::cin >> opt >> u >> v;
+        if (opt == 0) {
+            std::cin >> x;
+            std::cout << dsu.merge(u, v, x) << '\n';
+        } else if (!dsu.same(u, v)) {
+            std::cout << "-1\n";
+        } else {
+            std::cout << dsu.distance(u, v) << '\n';
+        }
+    }
+
+    return 0;
+}

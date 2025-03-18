340f0143c651d5f693fa748895b5d6854c0ec77f
FredB-mine <fredbao1126@gmail.com>
2025-03-18 23:19:06 +0800

    Test chtholly tree (not correct solution, but correct in random tests.)

--- src/alfred/data_structure/chtholly.hpp
+++ src/alfred/data_structure/chtholly.hpp
@@ -3,29 +3,34 @@
 
 #include <set>
 
+template <class T>
 struct ChthollyTree {
     using i64 = long long;
     struct Node {
-        mutable i64 l, r, v;
-        inline bool operator<(const Node &x) const { return l < x.l; }
+        mutable i64 l, r;
+        mutable T v;
+        bool operator<(const Node &x) const {
+            return l < x.l;
+        }
     };
     std::set<Node> tr;
-    using iterator = std::set<Node>::iterator;
+    using iterator = typename std::set<Node>::iterator;
     ChthollyTree(void) = default;
-    ChthollyTree(int rng, int val) { init(rng, val); }
-    inline void init(i64 rng, i64 val) noexcept {
-        tr.insert({1, rng, val}), tr.insert({rng + 1, rng + 1, 0});
+    ChthollyTree(int rng, T val = T{}) { init(rng, val); }
+    inline void init(i64 rng, T val = T{}) noexcept {
+        tr.insert({0, rng, val}), tr.insert({rng + 1, rng + 1, 0});
     }
     inline iterator begin(void) const noexcept { return tr.begin(); }
     inline iterator end(void) const noexcept { return tr.end(); }
     inline iterator split(i64 pos) {
         auto it = tr.lower_bound({pos, 0, 0});
         if (it != tr.end() && it->l == pos) return it;
-        i64 l = (--it)->l, r = it->r, v = it->v;
+        i64 l = (--it)->l, r = it->r;
+        const T v = it->v;
         tr.erase(it), tr.insert({l, pos - 1, v});
         return tr.insert({pos, r, v}).first;
     }
-    inline void assign(i64 l, i64 r, i64 v) {
+    inline void assign(i64 l, i64 r, T v) {
         auto R = split(r + 1), L = split(l);
         tr.erase(L, R), tr.insert({l, r, v});
     }
@@ -34,9 +39,9 @@ struct ChthollyTree {
         auto R = split(r + 1), L = split(l);
         for (auto it = L; it != R; it++) func(it);
     }
-    template <class _Functor> // func(i64 &, iterator)
-    inline i64 query(i64 l, i64 r, _Functor func) {
-        i64 ans = 0;
+    template <class _Functor> // func(T &, iterator)
+    inline T query(i64 l, i64 r, _Functor func) {
+        T ans{};
         auto R = split(r + 1);
         for (auto it = split(l); it != R; it++) func(ans, it);
         return ans;

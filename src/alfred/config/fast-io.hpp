70cb6eb9ab916ae0b47393894bde1d45ca6b7d5f
FredB-mine <fredbao1126@gmail.com>
2025-03-22 23:25:30 +0800

    Rewritten fast-io.hpp

--- src/alfred/config/fast-io.hpp
+++ src/alfred/config/fast-io.hpp
@@ -1,27 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-namespace fastIO {
-    char c, f, e = 0;
-    namespace usr {
-        template <class _Tp>
-        inline int read(_Tp &x) {
-            x = f = 0, c = getchar();
-            while (!isdigit(c) && !e) f = c == '-', e |= c == EOF, c = getchar();
-            while (isdigit(c) && !e) x = (x << 1) + (x << 3) + (c ^ 48), c = getchar();
-            return (e |= c == EOF) ? 0 : ((f ? x = -x : 0), 1);
-        }
-        template <class _Tp>
-        inline void write(_Tp x) {
-            if (x < 0) putchar('-'), x = -x;
-            if (x > 9) write(x / 10);
-            putchar((x % 10) ^ 48);
-        }
-        template <typename T, typename... V>
-        inline void read(T &t, V &...v) { read(t), read(v...); }
-        template <typename T, typename... V>
-        inline void write(T t, V... v) {
-            write(t), putchar(' '), write(v...);
-        }
+class FastIO {
+private:
+    int siz = 100000;
+    char *buf, *p1, *p2;
+
+public:
+    inline char nc(void) {
+        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, siz, stdin), p1 == p2) ? EOF : *p1++;
+    }
+    FastIO(void) { buf = (char *)malloc(siz); }
+    FastIO(int n) : siz(n) { buf = (char *)malloc(siz); } // n characters.
+    ~FastIO(void) { free(buf); }
+    inline void resize(int n) {
+        siz = n;
+        buf = (char *)realloc(buf, siz);
+    }
+} __buf;
+
+inline void reset_size(int siz) {
+    __buf.resize(siz);
+}
+
+template <class T>
+inline void fast_read(T &x) {
+    x = 0;
+    static char c = __buf.nc();
+    while (c < '0' || c > '9') c = __buf.nc();
+    while (c >= '0' && c <= '9') {
+        x = (x << 1) + (x << 3) + (c ^ 48), c = __buf.nc();
     }
 }
-using namespace fastIO::usr;
+template <class T>
+inline void write(T x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x > 9) write(x / 10);
+    putchar((x % 10) ^ 48);
+}
+template <class T>
+inline void writeln(T x) { write(x), puts(""); }
+template <typename T, typename... V>
+inline void fast_read(T &t, V &...v) { fast_read(t), fast_read(v...); }
--- verify/verify-aizu-graph/aizu-lca.test.cpp
+++ verify/verify-aizu-graph/aizu-lca.test.cpp
@@ -1,5 +1,6 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_C"
 
+#include "../../src/alfred/config/fast-io.hpp"
 #include "../../src/alfred/config/io-sync-off.hpp"
 #include <iostream>
 
@@ -9,18 +10,19 @@ std::vector<int> G[100010];
 #include "../../src/alfred/graph/lca.hpp"
 
 int main(int argc, char const *argv[]) {
-    optimizeIO(), std::cin >> n;
+    optimizeIO(), fast_read(n);
     for (int i = 0; i < n; i++) {
-        std::cin >> k;
+        fast_read(k);
         while (k--) {
-            std::cin >> c;
+            fast_read(c);
             G[i].push_back(c);
         }
     }
-    LCA.init(n, 0), std::cin >> q;
+    LCA.init(n, 0);
+    fast_read(q);
     while (q--) {
-        std::cin >> u >> v;
-        std::cout << LCA.LCA(u, v) << '\n';
+        fast_read(u, v);
+        writeln(LCA.LCA(u, v));
     }
     return 0;
 }

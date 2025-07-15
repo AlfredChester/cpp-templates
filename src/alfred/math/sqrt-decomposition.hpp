29264ff8be51bbc8ee5c61e49aba9091a00bd87a
AlfredChester <fredbao1126@gmail.com>
2025-07-15 20:54:32 +0800

    Enhanced sqrt-decomposition, added i128 iostream.

--- src/alfred/math/sqrt-decomposition.hpp
+++ src/alfred/math/sqrt-decomposition.hpp
@@ -7,9 +7,9 @@
 template <class T>
 std::vector<std::array<T, 3>> sqrt_decomposit(T n) { // (l, r, v)
     std::vector<std::array<T, 3>> ans;
-    for (T l = 1, r; l <= n; l = r + 1) {
-        r = n / (n / l);
-        ans.push_back({l, r, n / l});
+    for (T l = 1, r, v; l <= n; l = r + 1) {
+        v = n / l, r = n / v;
+        ans.push_back({l, r, v});
     }
     return ans;
 }

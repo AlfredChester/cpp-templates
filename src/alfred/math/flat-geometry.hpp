f7bdb7916eeb0c38c6b03ce32612103d9133923e
AlfredChester <fredbao1126@gmail.com>
2025-12-07 09:09:29 +0800

    Half-done flat geometry utilities

--- src/alfred/math/flat-geometry.hpp
+++ src/alfred/math/flat-geometry.hpp
@@ -2,4 +2,41 @@
 #ifndef AFMT_FLAT_GEOMETRY
 #define AFMT_FLAT_GEOMETRY
 
+#include <cmath>
+#include <iostream>
+
+template <class T>
+struct Vec {
+    T x, y;
+    Vec(T _x = 0, T _y = 0) : x(_x), y(_y) {}
+    inline T norm(void) const { return x * x + y * y; }
+    inline Vec operator*(T &&d) const { return Vec(x * d, y * d); }
+    inline Vec operator/(T &&d) const { return Vec(x / d, y / d); }
+    inline Vec operator+(Vec &&v) const { return Vec(x + v.x, y + v.y); }
+    inline Vec operator-(Vec &&v) const { return Vec(x - v.x, y - v.y); }
+    friend std::ostream &operator<<(std::ostream &out, Vec &&P) {
+        return out << "(" << P.x << ", " << P.y << ")";
+    }
+    friend std::istream &operator>>(std::istream &in, Vec &P) {
+        return in >> P.x >> P.y;
+    }
+};
+
+template <class T>
+using Point = Vec<T>;
+
+template <class T>
+inline T dot(Vec<T> &&a, Vec<T> &&b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+template <class T>
+inline T cross(Vec<T> &&a, Vec<T> &&b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+template <class T>
+struct Segment {
+};
+
 #endif

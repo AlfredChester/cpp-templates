e12f71b806efcb831e122551360dfd4d7e0fb097
AlfredChester <fredbao1126@gmail.com>
2025-12-19 19:54:01 +0800

    fix: fixed a static assert in radix sort

--- src/alfred/algorithm/radix-sort.hpp
+++ src/alfred/algorithm/radix-sort.hpp
@@ -16,7 +16,6 @@
 
 template <class T, const unsigned w = 8>
 static void radix_sort(T a[], const size_t n) {
-    static_assert(tot % (2 * w) == 0, "The width of type must be divisible by 2w.");
     static std::vector<T> b_vec;
     if (b_vec.size() < n) {
         b_vec.resize(n);
@@ -24,6 +23,7 @@ static void radix_sort(T a[], const size_t n) {
     T *b = b_vec.data();
     static unsigned bucket[1 << w];
     const unsigned tot = sizeof(T) * 8;
+    static_assert(tot % (2 * w) == 0, "The width of type must be divisible by 2w.");
     for (unsigned d1 = 0, d2 = w; d2 < tot;) {
         _SORTBYTE(T, a, b, n, d1, w);
         _SORTBYTE(T, b, a, n, d2, w);

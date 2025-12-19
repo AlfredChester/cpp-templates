ddef6a2a704d5768756b7b284ad6a310588d5d22
AlfredChester <fredbao1126@gmail.com>
2025-12-19 19:37:35 +0800

    feat: finished add_conv_ll

--- src/alfred/math/conv.hpp
+++ src/alfred/math/conv.hpp
@@ -38,6 +38,7 @@ inline std::vector<ModInt<mod>> add_conv(
     std::vector<ModInt<mod>> f,
     std::vector<ModInt<mod>> g
 ) {
+    if (f.empty() || g.empty()) return {};
     int len = f.size() + g.size() - 1, lim;
     for (lim = 1; lim < len; lim <<= 1);
     f.resize(lim, 0), g.resize(lim, 0);
@@ -57,26 +58,51 @@ inline std::vector<ModInt<mod>> add_conv(
     return h.resize(len), h;
 }
 
-/*
 // (*, +) convolution for long long
-inline std::vector<long long> add_convll(
-    std::vector<long long> f, std::vector<long long> g
+inline std::vector<long long> add_conv_ll(
+    const std::vector<long long> &f,
+    const std::vector<long long> &g
 ) {
-    std::vector<ModInt<998244353>> f1(f.begin(), f.end());
-    std::vector<ModInt<998244353>> g1(g.begin(), g.end());
-    std::vector<ModInt<998244353>> h1 = add_conv(f1, g1);
-
-    std::vector<ModInt<469762049>> f2(f.begin(), f.end());
-    std::vector<ModInt<469762049>> g2(g.begin(), g.end());
-    std::vector<ModInt<469762049>> h2 = add_conv(f2, g2);
-
-    std::vector<ModInt<1004535809>> f3(f.begin(), f.end());
-    std::vector<ModInt<1004535809>> g3(g.begin(), g.end());
-    std::vector<ModInt<1004535809>> h3 = add_conv(f3, g3);
-
-    std::vector<T> res(h1.size());
+    if (f.empty() || g.empty()) return {};
+    static constexpr uint64_t m1 = 754974721;
+    static constexpr uint64_t m2 = 167772161;
+    static constexpr uint64_t m3 = 469762049;
+    static constexpr uint64_t m12 = m1 * m2;
+    static constexpr uint64_t m13 = m1 * m3;
+    static constexpr uint64_t m23 = m2 * m3;
+    static constexpr uint64_t m123 = m1 * m2 * m3;
+    auto c1 = add_conv(
+        std::vector<ModInt<m1>>(f.begin(), f.end()),
+        std::vector<ModInt<m1>>(g.begin(), g.end())
+    );
+    auto c2 = add_conv(
+        std::vector<ModInt<m2>>(f.begin(), f.end()),
+        std::vector<ModInt<m2>>(g.begin(), g.end())
+    );
+    auto c3 = add_conv(
+        std::vector<ModInt<m3>>(f.begin(), f.end()),
+        std::vector<ModInt<m3>>(g.begin(), g.end())
+    );
+    const int len = c1.size();
+    std::vector<long long> c(len);
+    static constexpr uint64_t i1 = ModInt<m1>(m2 * m3).inv().val();
+    static constexpr uint64_t i2 = ModInt<m2>(m1 * m3).inv().val();
+    static constexpr uint64_t i3 = ModInt<m3>(m1 * m2).inv().val();
+    for (int i = 0; i < len; i++) {
+        uint64_t x = 0;
+        x += (c1[i].val() * i1) % m1 * m23;
+        x += (c2[i].val() * i2) % m2 * m13;
+        x += (c3[i].val() * i3) % m3 * m12;
+        long long diff = c1[i].val() - (long long)(x % m1);
+        if (diff < 0) diff += m1;
+        if (diff >= (long long)m1) diff -= m1;
+        static constexpr uint64_t offset[5] = {
+            0, 0, m123, 2 * m123, 3 * m123
+        };
+        c[i] = x - offset[diff % 5];
+    }
+    return c;
 }
-*/
 
 template <uint32_t mod>
 inline std::vector<ModInt<mod>> and_conv(

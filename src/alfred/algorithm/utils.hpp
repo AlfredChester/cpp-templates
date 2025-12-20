ea43c5bdb2190a70c9b1399c750413e2a9fc32df
AlfredChester <fredbao1126@gmail.com>
2025-12-20 22:56:31 +0800

    feat: added bin, hex, and range_traverse to utils

--- src/alfred/algorithm/utils.hpp
+++ src/alfred/algorithm/utils.hpp
@@ -55,4 +55,74 @@ std::vector<T> rand_seq(size_t n, size_t seed = 20090913) {
     return res;
 }
 
+template <class T>
+std::vector<std::vector<T>> range_traverse(size_t n, T l, T r) {
+    std::vector<T> cur(n);
+    std::vector<std::vector<T>> res;
+    auto dfs = [&](auto &self, size_t depth) {
+        if (depth == n) {
+            res.push_back(cur);
+            return;
+        }
+        for (T i = l; i <= r; i++) {
+            cur[depth] = i;
+            self(self, depth + 1);
+        }
+    };
+    dfs(dfs, 0);
+    return res;
+}
+
+template <class T>
+inline std::string bin(T v) {
+    std::string s;
+    if (v == 0) return "0b0";
+    while (v > 0) {
+        s += v & 1 ? '1' : '0', v >>= 1;
+    }
+    s += "b0";
+    std::reverse(s.begin(), s.end());
+    return s;
+}
+
+template <class T>
+inline std::string bin(T v, int dig) {
+    std::string s = "0b";
+    for (int i = dig - 1; i >= 0; i--) {
+        s += (v >> i & 1) ? '1' : '0';
+    }
+    return s;
+}
+
+inline char hex(int v) {
+    static const char tab[16]{
+        '0', '1', '2', '3',
+        '4', '5', '6', '7',
+        '8', '9', 'a', 'b',
+        'c', 'd', 'e', 'f'
+    };
+    return tab[v];
+}
+
+template <class T>
+inline std::string hex(T v) {
+    std::string s;
+    if (v == 0) return "0x0";
+    while (v > 0) {
+        s += hex(v & 15), v >>= 4;
+    }
+    s += "x0";
+    std::reverse(s.begin(), s.end());
+    return s;
+}
+
+template <class T>
+inline std::string hex(T v, int dig) {
+    std::string s = "0x";
+    for (int i = dig - 1; i >= 0; i--) {
+        s += hex(v >> (4 * i) & 15);
+    }
+    return s;
+}
+
 #endif // AFALG_UTILS

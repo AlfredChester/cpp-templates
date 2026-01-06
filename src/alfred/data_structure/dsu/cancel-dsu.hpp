bf666f7c5451eb20785af958d91b8a597b9657e7
AlfredChester <fredbao1126@gmail.com>
2026-01-06 20:52:42 +0800

    chore: use std::vector instead of std::stack in CancelDSU

--- src/alfred/data_structure/dsu/cancel-dsu.hpp
+++ src/alfred/data_structure/dsu/cancel-dsu.hpp
@@ -2,13 +2,11 @@
 #define AFDS_CANCEL_DSU
 
 #include <numeric>
-#include <stack>
 #include <vector>
 
 class CancelDSU {
 private:
-    std::stack<int> S;
-    std::vector<int> fa, siz;
+    std::vector<int> fa, siz, S;
 
 public:
     CancelDSU(int n) : fa(n + 1), siz(n + 1, 1) {
@@ -23,15 +21,15 @@ public:
     inline int size(int u) { return siz[find(u)]; }
     inline void merge(int u, int v) {
         int fu = find(u), fv = find(v);
-        if (fu == fv) return S.push(-1);
+        if (fu == fv) return S.push_back(-1);
         if (siz[fu] < siz[fv]) std::swap(fu, fv);
-        siz[fu] += siz[fv], fa[fv] = fu, S.push(fv);
+        siz[fu] += siz[fv], fa[fv] = fu, S.push_back(fv);
     }
     inline void _cancel(void) {
         if (S.empty()) return;
-        if (S.top() == -1) return S.pop();
-        siz[fa[S.top()]] -= siz[S.top()];
-        fa[S.top()] = S.top(), S.pop();
+        if (S.back() == -1) return S.pop_back();
+        siz[fa[S.back()]] -= siz[S.back()];
+        fa[S.back()] = S.back(), S.pop_back();
     }
     inline void cancel(int t = 1) {
         while (t--) _cancel();
